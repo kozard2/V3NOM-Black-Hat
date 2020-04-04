08008763dc8048ecca66fba7849d6fc8ca8fce02
Mr.venom <63120931+kozard2@users.noreply.github.com>
2020-04-04 17:59:08 +0700

    Add files via upload

---
+++ venom.py
@@ -0,0 +1,46 @@
+import os
+import sys
+
+os.system('clear')
+os.system('figlet Hack FB')
+print "========================================================="
+print " Author: Mr.venom"
+print " youtube: Awuu Zans"
+print " github: https://github.com/kozard2/Mr.venom-Black-Hat"
+print "========================================================="
+print
+print
+print "{•} Menu Pilihan {•}"
+print "{1} Dark Fbv 1.7"
+print "{2} Dark Fbv 1.8"
+print "{3} Dark FB premium"
+print "{4} Dark Fbv 2.0"
+print "{5} Install Bahan"
+Pilih = raw_input('{?} Pilih : ')
+if Pilih == "1":
+        os.system('git clone git clone https://github.com/kozard2/Mr.venom-Black-Hat/DarkVen ')
+        print "{+} Penginstalan Selesai"
+        print "{+} Selanjutnya Ketik $ cd DarkVen"
+        print "{+} Lalu ketik $ python2 darkfb.py"
+elif Pilih == "2":
+        os.system('git clone https://github.com/kozard2/Mr.venom-Black-Hat/darkven ')
+        print "{+} Penginstalan Selesai"
+        print "{+} Selanjutnya Ketik $ cd darkven"
+        print "{+} Lalu ketik $ python2 darkFB.py"
+elif Pilih == "3":
+        os.system('git clone https://github.com/kozard2/Mr.venom-Black-Hat/DarkPremium ')
+        print "{+} Penginstalan Selesai"
+        print "{+} Selanjutnya Ketik $ cd DarkPremium"
+        print "{+} Lalu ketik $ python2 DarkFBpremium.py"
+elif Pilih == "4":
+        os.system('git clone git clone https://github.com/kozard2/Mr.venom-Black-Hat/Dark-FB ')
+        print "{+} Penginstalan Selesai"
+        print "{+} Selanjutnya Ketik $ cd Dark-FB"
+        print "{+} Lalu ketik $ python2 DarkFB.py"
+elif Pilih == "5":
+        os.system('apt update &&apt upgrade')
+        os.system('pkg install python2')
+        os.system('pkg install nano')
+        os.system('pkg install git')
+        os.system('pip2 install mechanize requests')
+        print "{+} Penginstalan Selesai"
